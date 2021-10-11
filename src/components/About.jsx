dd9f2e0384fcae13526671214c1c9f6b7efe589c
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-11 16:23:52 +0300

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const About = () => {
     return (
         <div>
-            
+            <div className="container">
+                <div className="row">
+                    <h1>About Us</h1>
+                    <p className="lead">
+                        Its us
+                    </p>
+
+                    <NavLink to = "/contact" className="btn btn-outline-primary">Contact Us</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
