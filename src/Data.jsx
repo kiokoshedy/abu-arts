490c20991214df4048b37b4c2b4b748200e08a38
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-11 15:35:00 +0300

    bugs

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,23 +1,95 @@
-const DATA =[
+const DATA = [
     {
         id : 0,
-        tittle : "Bottle Art",
+        title : "Bottle Art",
         price : 500,
         desc : "beauty",
-        img : "/images/img10.jpg"
+        img : "/images/img.jpg"
     },
     {
         id : 1,
-        tittle : "Bottle Art",
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img0.jpg"
+    },
+    {
+        id : 2,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img1.jpg"
+    },
+    {
+        id : 3,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img2.jpg"
+    },
+    {
+        id : 4,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img3.jpg"
+    },
+    {
+        id : 5,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img4.jpg"
+    },
+    {
+        id : 6,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img5.jpg"
+    },
+    {
+        id : 7,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img6.jpg"
+    },
+    {
+        id : 8,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img7.jpg"
+    },
+    {
+        id : 9,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img8.jpg"
+    },
+    {
+        id : 10,
+        title : "Bottle Art",
         price : 500,
         desc : "beauty",
         img : "/images/img9.jpg"
     },
     {
-        id : 2,
-        tittle : "Bottle Art",
+        id : 11,
+        title : "Bottle Art",
+        price : 500,
+        desc : "beauty",
+        img : "/images/img10.jpg"
+    },
+    {
+        id : 12,
+        title : "Bottle Art",
         price : 500,
         desc : "beauty",
         img : "/images/img11.jpg"
     }
 ]
+
+export default DATA
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -6,13 +6,13 @@ const Product = () => {
 
     const cardItem = (item) => {
         return (
-            <div class="card my-5 py-4" key={item.id} style={{ width: "18rem" }}>
+            <div class="card my-5 py-5" key={item.id} style={{width: "18rem" }}>
                 <img src={item.img} class="card-img-top" alt={item.tittle} />
                 <div class="card-body text-center">
                     <h5 class="card-title">{item.tittle}</h5>
                     <h5 class="card-title">{item.desc}</h5>
-                    <p className="lead">${item.price}</p>
-                    <NavLink to = {'/products/${item.id}'} class="btn btn-outline-primary">Buy Now</NavLink>
+                    <p className="lead">Kes {item.price}</p>
+                    <NavLink to = {`/products/${item.id}`} class="btn btn-outline-primary">Buy Now</NavLink>
                 </div>
             </div>
         );
@@ -22,12 +22,12 @@ const Product = () => {
             <div className="container py-5">
                 <div className="row">
                     <div className="col-12 text-center">
-                        <h1>Product</h1>
+                        <h1>Products</h1>
                         <hr />
                     </div>
                 </div>
                 <div className="container">
-                    <div className="row justfy-content-around">
+                    <div className="row justify-content-around">
                         {DATA.map(cardItem)}
                     </div>
                 </div>
