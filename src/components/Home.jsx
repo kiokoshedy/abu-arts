e80c099fda68a7deee17d6942ae7b00e606212c6
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-11 13:06:50 +0300

    homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,16 +8,20 @@ const Home = () => {
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
                 </div>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="..." class="d-block w-100" alt="..."/>
+                        <img src="/images/img.jpg" class="d-block w-100" alt="Art" height="600px"/>
                     </div>
                     <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..."/>
+                        <img src="/images/img1.jpg" class="d-block w-100" alt="Art" height="600px"/>
                     </div>
                     <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="..."/>
+                        <img src="/images/img2.jpg" class="d-block w-100" alt="Art" height="600px"/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/images/img3.jpg" class="d-block w-100" alt="Art" height="600px" />
                     </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
