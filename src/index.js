2b0430917787954ab5942349c0093c5dbf6b6843
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-12 12:55:47 +0300

    bugs

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import 'font-awesome/css/font-awesome.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
