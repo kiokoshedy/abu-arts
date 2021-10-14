e216b863a03e8e2edaf9e31825e18490702591b6
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-14 11:14:38 +0300

    checkout

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,13 +9,13 @@ const About = () => {
                     <div className="col-md-6">
                         <h1 className="text-primary fw-bold mb-4">About Us</h1>
                         <p className="lead mb-4">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, impedit. Voluptas, odio! Eligendi mollitia reprehenderit inventore dolorum laudantium, eos, dolorem libero repudiandae odit natus dolores soluta fugiat expedita quo, ducimus sit deleniti tenetur quas consequatur laboriosam nam! Iure ullam, placeat unde aut modi dolorum expedita suscipit doloremque quaerat beatae cupiditate minima quos eveniet perferendis porro soluta minus dolorem quod quisquam, sint odio. Velit laborum distinctio totam sint modi aperiam suscipit, sunt dolore quia explicabo neque maiores error consequuntur ea. Cum temporibus fugit obcaecati aliquid odio praesentium, cupiditate et modi aperiam, ducimus beatae corrupti totam in repellat hic odit eius voluptatum?
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi temporibus culpa repellat laborum ipsa officia minima voluptatem quo ducimus quas dicta debitis, at velit iusto quia qui repudiandae magnam sed.
                         </p>
-                        <NavLink to = "/contact" className="btn btn-outline-primary px-3">Contact Us</NavLink>
+                        <NavLink to="/contact" className="btn btn-outline-primary px-3">Contact Us</NavLink>
                     </div>
                     <div className="col-md-6 d-flex justify-content-center">
                         <img src="/images/img.jpg" alt="About Us"
-                        height="400px" width="300px"/>
+                            height="400px" width="300px" />
                     </div>
                 </div>
             </div>
