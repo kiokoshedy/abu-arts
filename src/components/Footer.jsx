f2a57d727462191c68ae3e576c39f7b925e67d3e
Shady Kioko <kiokomusembi1@gmail.com>
2021-10-19 18:18:23 +0300

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <br/>
+            <div class="container-fluid py-5">
+
+                <div class="row d-flex justify-content-center">
+
+                    <div class="col-md-6">
+
+                        <div class="embed-responsive embed-responsive-16by9 mb-4">
+                            
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            <div class="footer-copyright text-center py-3">© 2021 Copyright:
+                <a href="https://abuartsdecor.com/"> abuartsdecor.com</a>
+            </div>
+        </>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,42 +3,47 @@ import { NavLink } from 'react-router-dom'
 import CartBtn from './buttons/CartBtn'
 import Login from './buttons/Login'
 import Signup from './buttons/Signup'
+import logo from '../images/logo.jpg';
 
 const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light ">
                 <div className="container-fluid py-2">
-                    
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink className="nav-link active" aria-current="page" 
-                                to="/">HOME</NavLink>
+                                <img src={logo} alt="About Us"
+                                    height="40px" width="15px" />
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" 
-                                to="/products">PRODUCTS</NavLink>
+                                <NavLink className="nav-link active" aria-current="page"
+                                    to="/">HOME</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" 
-                                to="/about">ABOUT</NavLink>
+                                <NavLink className="nav-link"
+                                    to="/products">PRODUCTS</NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink className="nav-link" 
-                                to="/contact">CONTACTS</NavLink>
+                                <NavLink className="nav-link"
+                                    to="/about">ABOUT</NavLink>
                             </li>
-                            
+                            <li className="nav-item">
+                                <NavLink className="nav-link"
+                                    to="/contact">CONTACTS</NavLink>
+                            </li>
+
                         </ul>
-                        
-                    
-                    <NavLink className="navbar-brand mx-auto fw-bold" to="#">ABUARTS DECOR</NavLink>
-                    <Login/>
-                    <Signup/>
-                    <CartBtn/>
+
+
+                        <NavLink className="navbar-brand mx-auto fw-bold" to="#">ABUARTS DECOR</NavLink>
+                        <Login />
+                        <Signup />
+                        <CartBtn />
                     </div>
                 </div>
             </nav>
