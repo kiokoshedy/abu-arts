3c902d3e124037bf508f9fb839c3ac01cbb40ab0
Shady Kioko <kiokomusembi1@gmail.com>
2022-04-21 12:02:36 +0300

    add mpesa

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -150,37 +150,22 @@ const Checkout = () => {
 
                             <div class="row gy-3">
                                 <div class="col-md-6">
-                                    <label for="cc-name" class="form-label">Name on card</label>
+                                    <label for="cc-name" class="form-label">Your Name</label>
                                     <input type="text" class="form-control" id="cc-name" placeholder="" required=""/>
-                                        <small class="text-muted">Full name as displayed on card</small>
+                                        <small class="text-muted">Full name used for Mpesa.</small>
                                         <div class="invalid-feedback">
-                                            Name on card is required
+                                            Mpesa name is required
                                         </div>
                                 </div>
 
                                 <div class="col-md-6">
-                                    <label for="cc-number" class="form-label">Credit card number</label>
+                                    <label for="cc-number" class="form-label">Mpesa number</label>
                                     <input type="text" class="form-control" id="cc-number" placeholder="" required=""/>
                                         <div class="invalid-feedback">
-                                            Credit card number is required
+                                        Mpesa Number is required
                                         </div>
                                 </div>
 
-                                <div class="col-md-3">
-                                    <label for="cc-expiration" class="form-label">Expiration</label>
-                                    <input type="text" class="form-control" id="cc-expiration" placeholder="" required=""/>
-                                        <div class="invalid-feedback">
-                                            Expiration date required
-                                        </div>
-                                </div>
-
-                                <div class="col-md-3">
-                                    <label for="cc-cvv" class="form-label">CVV</label>
-                                    <input type="text" class="form-control" id="cc-cvv" placeholder="" required=""/>
-                                        <div class="invalid-feedback">
-                                            Security code required
-                                        </div>
-                                </div>
                             </div>
 
                             <hr className="my-4" />
